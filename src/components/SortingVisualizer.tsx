800e4c340fe43377b843a756489661ccf1c11907
Fabian <fabian.tilmans@gmx.de>
2021-07-15 22:36:43 +0200

    rework and implementation of trace

--- src/components/SortingVisualizer.tsx
+++ src/components/SortingVisualizer.tsx
@@ -1,1 +1,34 @@
-export default [];
+import React, { Component, Fragment } from "react";
+import { TraceItem } from "../types/Trace";
+import Chart from "./Chart";
+
+type Props = {
+    trace: TraceItem[];
+    array: number[];
+    arraySize: number;
+};
+
+type State = { 
+    trace: TraceItem[];
+};
+
+class SortingVisualiszer extends Component<Props, State> {
+    render(){
+
+        return(
+            <Fragment>
+                <section className="controls">
+
+                </section>
+                <section className="chart">
+                    <Chart array={this.props.array} arraySize={this.props.arraySize}/>
+                </section>
+                <section className="desciption">
+
+                </section>
+            </Fragment>
+        )
+    }
+}
+
+export default SortingVisualiszer;
