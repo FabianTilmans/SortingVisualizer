4c3184e08abb651e15fcbaba5a155d608cea5ce1
Fabian <fabian.tilmans@gmx.de>
2021-07-16 00:54:00 +0200

    added visualizer logic

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,17 +1,24 @@
 import React, { FC } from "react";
-import Bar from "./elements/Bar";
+import Bar from "./elements/Bar/Bar";
 
 type Props = {
     array: number[];
     arraySize: number;
+
+    currentSwapped: number[],
+    currentCompared: number [],
+    sorted: number[],
 };
 
-const Chart: FC<Props> = ({ array, arraySize }) => {
+const Chart: FC<Props> = ({ array, arraySize, currentCompared, currentSwapped, sorted }) => {
     const max: number = Math.max(...array);
 
     const bars = array.map((item, i) => {
         const width: number = 100 / arraySize;
         const height: number = (item / max) * 100;
+        const swapping: boolean = currentSwapped.includes(i);
+        const comparing: boolean = currentCompared.includes(i);
+        const _sorted: boolean = sorted.includes(i);
 
         const margin: string = i === arraySize ? "0" : width > 3 ? "0.5rem" : "0.125rem";
 
@@ -21,6 +28,9 @@ const Chart: FC<Props> = ({ array, arraySize }) => {
                 width={width}
                 height={height}
                 val={item}
+                swapping={swapping}
+                comparing={comparing}
+                sorted={_sorted}
                 style={{ marginRight: `${margin}` }}
             />
         );
--- src/components/SortingVisualizer.tsx
+++ src/components/SortingVisualizer.tsx
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from "react";
 import { TraceItem } from "../types/Trace";
 import Chart from "./Chart";
+import Controls from "./Controls";
 
 type Props = {
     trace: TraceItem[];
@@ -8,26 +9,143 @@ type Props = {
     arraySize: number;
 };
 
-type State = { 
+type State = {
     trace: TraceItem[];
+    traceStep: number;
+
+    array: number[];
+    originalArray: number[];
+    currentSwapped: number[];
+    currentCompared: number[];
+    sorted: number[];
+
+    timeoutIds: NodeJS.Timeout[];
+    speed: number;
 };
 
 class SortingVisualiszer extends Component<Props, State> {
-    render(){
+    state: State = {
+        trace: [],
+        traceStep: -1,
 
-        return(
+        array: [],
+        originalArray: [],
+        currentSwapped: [],
+        currentCompared: [],
+        sorted: [],
+
+        timeoutIds: [],
+        speed: 1,
+    };
+
+    componentDidUpdate(prevProps: Props) {
+        if (prevProps.array !== this.props.array) {
+            this.reset(this.props.array);
+        }
+        if (prevProps.trace !== this.props.trace) {
+            this.clearTimeouts();
+            this.setState({ trace: this.props.trace });
+        }
+    }
+
+    clearTimeouts = () => {
+        this.state.timeoutIds.forEach((timeoutid) => clearTimeout(timeoutid));
+        this.setState({ timeoutIds: [] });
+    };
+
+    run = (trace: TraceItem[]): void => {
+        const timeoutIds = [];
+        const timer = 250 / this.state.speed;
+
+        trace.forEach((item, i) => {
+            const timeout = setTimeout(
+                (item: TraceItem) => {
+                    this.setState(
+                        (prevState) => ({
+                            traceStep: prevState.traceStep + 1,
+                        }),
+                        () => this.displayTraceStep(item)
+                    );
+                },
+                i * timer,
+                item
+            );
+
+            timeoutIds.push(timeout);
+        });
+
+        const timeoutId = setTimeout(this.clearTimeouts, trace.length * timer);
+
+        timeoutIds.push(timeoutId);
+
+        this.setState({ timeoutIds });
+    };
+
+    pause = () => {
+        this.clearTimeouts();
+    };
+
+    continue = () => {
+        const step = this.state.trace.slice(this.state.traceStep);
+        this.run(step);
+    };
+
+    repeat = () => {
+        this.clearTimeouts();
+        this.setState((prevState) => ({
+            array: [...prevState.originalArray],
+            traceStep: -1,
+            currentCompared: [],
+            currentSwapped: [],
+            sorted: [],
+        }));
+        this.run(this.state.trace);
+    };
+
+    reset = (array: number[]) => {
+        this.setState({
+            array,
+            trace: [],
+            traceStep: -1,
+            currentSwapped: [],
+            currentCompared: [],
+            sorted: [],
+            originalArray: [...array],
+        });
+    };
+
+    displayTraceStep = (step: TraceItem): void => {
+        this.setState({
+            array: step.array,
+            sorted: step.sorted,
+            currentSwapped: step.swapped,
+            currentCompared: step.compared,
+        });
+    };
+
+    render() {
+        return (
             <Fragment>
-                <section className="controls">
-
-                </section>
+                <section className="controls"></section>
+                <Controls
+                    onStart={
+                        this.state.traceStep === -1
+                            ? this.run.bind(this, this.state.trace)
+                            : this.continue.bind(this)
+                    }
+                />
                 <section className="chart">
-                    <Chart array={this.props.array} arraySize={this.props.arraySize}/>
+                    <Chart
+                        array={this.state.array}
+                        arraySize={this.props.arraySize}
+                        currentCompared={this.state.currentCompared}
+                        currentSwapped={this.state.currentSwapped}
+                        sorted={this.state.sorted}
+                    />
                 </section>
-                <section className="desciption">
-
-                </section>
+                <section className="desciption"></section>
             </Fragment>
-        )
+        );
     }
 }
 
